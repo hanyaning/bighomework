866b7a84829d964768f5138f09ff0bfd81340383
hanyaning <1193985098@qq.com>
2018-12-29 15:15:06 +0800

    message

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+var box = document.getElementById("cover19");
+var oNavlist = document.getElementById("nav").children;
+var slider = document.getElementById('slider');
+var left = document.getElementById('left');
+var right = document.getElementById('right');
